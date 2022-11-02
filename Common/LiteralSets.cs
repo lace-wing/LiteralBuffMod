2ea058c259e67cecb831786146cc242da33d4625
lyc-Lacewing <lycrlsu01@gmail.com>
2022-11-02 15:08:11 +0800

    AquaticNPC HashSet implementation

--- Common/LiteralSets.cs
+++ Common/LiteralSets.cs
@@ -11,7 +11,39 @@ namespace LiteralBuffMod.Common
     {
         //internal static bool[] aquaticNPCTypes = new bool[NPCID.Count];
         //internal static int[] vanillaAquaticNPCIds = new int[] { NPCID.AnglerFish, NPCID.DukeFishron, NPCID.EyeballFlyingFish, NPCID.FungoFish, NPCID.FlyingFish, NPCID.BlueJellyfish, NPCID.GreenJellyfish, NPCID.PinkJellyfish, NPCID.IcyMerman, NPCID.ZombieMerman, NPCID.CreatureFromTheDeep };
-        internal static HashSet<int> aquaticNPCType = new HashSet<int>() { NPCID.AnglerFish, NPCID.DukeFishron, NPCID.EyeballFlyingFish, NPCID.FungoFish, NPCID.FlyingFish, NPCID.BlueJellyfish, NPCID.GreenJellyfish, NPCID.PinkJellyfish, NPCID.IcyMerman, NPCID.ZombieMerman, NPCID.CreatureFromTheDeep };
+        internal static HashSet<int> aquaticNPCType = new HashSet<int>()
+        {
+            //Fish
+            NPCID.AnglerFish, NPCID.DukeFishron, NPCID.EyeballFlyingFish, NPCID.FungoFish, NPCID.FlyingFish, 
+            //Jellyfish
+            NPCID.BlueJellyfish, NPCID.GreenJellyfish, NPCID.PinkJellyfish, 
+            //Merman
+            NPCID.IcyMerman, NPCID.ZombieMerman, NPCID.CreatureFromTheDeep, 
+            //Shark
+            NPCID.Shark, NPCID.GoblinShark, NPCID.Sharkron, NPCID.Sharkron2, NPCID.SandShark, NPCID.SandsharkCorrupt, NPCID.SandsharkCrimson, NPCID.SandsharkHallow, 
+            //Goldfish
+            NPCID.CorruptGoldfish, NPCID.CrimsonGoldfish, NPCID.Goldfish, NPCID.GoldfishWalker, NPCID.GoldGoldfish, NPCID.GoldGoldfishWalker, 
+            //Eel
+            NPCID.BloodEelHead, NPCID.BloodEelBody, NPCID.BloodEelTail, 
+            //Squid
+            NPCID.BloodSquid, NPCID.Squid, 
+            //Nautilus
+            NPCID.BloodNautilus, 
+            //Piranha
+            NPCID.Piranha, 
+            //Sea Snail
+            NPCID.SeaSnail, 
+            //Crab
+            NPCID.Crab, 
+            //Crawdad
+            NPCID.Crawdad, NPCID.Crawdad2, 
+            //Arapaima
+            NPCID.Arapaima, 
+            //Pigron
+            NPCID.PigronCorruption, NPCID.PigronCrimson, NPCID.PigronHallow, 
+            //Swamp Thing
+            NPCID.SwampThing
+        };
 
         internal static void SetUpSets()
         {
